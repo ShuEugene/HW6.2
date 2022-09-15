f74e11e95681c8682d8e0edf202ddb8fb571c81a
ShuEugene <shueugene@hotmail.com>
2022-09-15 12:50:51 +0300

    HW6.2.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
 //  ДЗ к уроку 6.2 «Массивы»
 public class Main {
     public static void main(String[] args) {
-        int[] monthlyExpenses = generateRandomArray();
+
+        System.out.println();
+        int dayInMonth = 30,
+                minExpenses = 100_000, maxExpenses = 200_000, rangeExpenses = maxExpenses - minExpenses + 1;
+        int[] daylyExpenses = generateRandomArray(dayInMonth, minExpenses, rangeExpenses);
+        System.out.printf("Сумма трат за месяц составила %s рублей.", calculateSumm(daylyExpenses));
 
         System.out.println();
     }
 
-    static int[] generateRandomArray() {
+    static int[] generateRandomArray(int arrayLenght, int minValue, int range) {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
+        int[] arr = new int[arrayLenght];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
+            arr[i] = random.nextInt(range) + minValue;
         }
         return arr;
+    }
+
+// showAray()
+/*    static void showArray(int[] someArray) {
+        for (int i = 0; i < someArray.length - 1; i++) System.out.printf("%s, ", someArray[i]);
+        System.out.printf("%s.\n", someArray[someArray.length - 1]);
+    }*/
+
+    static int calculateSumm(int[] expensesArray) {
+        int summ = 0;
+        for (int currentElement : expensesArray) summ += currentElement;
+        return summ;
     }
 }
