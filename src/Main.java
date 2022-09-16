e2d0bb30922f493582c7c34ce70c1f53166c0967
ShuEugene <shueugene@hotmail.com>
2022-09-16 10:38:24 +0300

    HW6.2 is fixed.

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,24 @@ public class Main {
 
 //  ДЗ6.2.1
         int[] dailyExpenses;
-        {
-            System.out.println();
-            int dayInMonth = 30,
-                    minExpenses = 100_000, maxExpenses = 200_000,
-                    rangeExpenses = maxExpenses - minExpenses + 1;
-            dailyExpenses = generateRandomArray(dayInMonth, minExpenses, rangeExpenses);
-            System.out.printf("Сумма трат за месяц составила %s рублей.\n", calculateSumm(dailyExpenses));
-        }
+        System.out.println();
+        int dayInMonth = 30, minExpenses = 100_000, maxExpenses = 200_000, rangeExpenses = maxExpenses - minExpenses + 1;
+        dailyExpenses = generateRandomArray(dayInMonth, minExpenses, rangeExpenses);
+        System.out.printf("Сумма трат за месяц составила %s рублей.\n", calculateSumm(dailyExpenses));
 
 //  ДЗ6.2.2
-        {
-            System.out.println();
-            System.out.printf("Минимальная сумма затрат за день составила %s рублей.\n" +
-                    "Максимальная сумма затрат за день составила %s рублей.\n",
-                    minDaylyExpenses(dailyExpenses), maxDaylyExpenses(dailyExpenses));
-        }
+        System.out.println();
+        System.out.printf("Минимальная сумма затрат за день составила %s рублей.\n" + "Максимальная сумма затрат за день составила %s рублей.\n", minDaylyExpenses(dailyExpenses), maxDaylyExpenses(dailyExpenses));
 
 //  ДЗ6.2.3
-        {
-            System.out.println();
-            System.out.printf("Средняя сумма трат за месяц составила %.2f рублей.\n",
-                    averageExpenses(dailyExpenses));
-        }
+        System.out.println();
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей.\n", averageExpenses(dailyExpenses));
 
 //  ДЗ6.2.4
-        {
-            System.out.println();
-            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-            for (int i = reverseFullName.length - 1; i >= 0; i--)
-                System.out.printf("%s", reverseFullName[i]);
+        System.out.println();
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.printf("%s", reverseFullName[i]);
         }
 
         System.out.println();
@@ -50,25 +38,37 @@ public class Main {
 
 // showAray()
 /*    static void showArray(int[] someArray) {
-        for (int i = 0; i < someArray.length - 1; i++) System.out.printf("%s, ", someArray[i]);
+        for (int i = 0; i < someArray.length - 1; i++) {
+            System.out.printf("%s, ", someArray[i]);
+        }
         System.out.printf("%s.\n", someArray[someArray.length - 1]);
     }*/
 
     static int calculateSumm(int[] expensesArray) {
         int summ = 0;
-        for (int currentElement : expensesArray) summ += currentElement;
+        for (int currentElement : expensesArray) {
+            summ += currentElement;
+        }
         return summ;
     }
 
     static int minDaylyExpenses(int[] dailyExpenses) {
         int min = dailyExpenses[0];
-        for (int curentElement : dailyExpenses) if (curentElement < min) min = curentElement;
+        for (int curentElement : dailyExpenses) {
+            if (curentElement < min) {
+                min = curentElement;
+            }
+        }
         return min;
     }
 
     static int maxDaylyExpenses(int[] dailyExpenses) {
         int max = dailyExpenses[0];
-        for (int curentElement : dailyExpenses) if (curentElement > max) max = curentElement;
+        for (int curentElement : dailyExpenses) {
+            if (curentElement > max) {
+                max = curentElement;
+            }
+        }
         return max;
     }
 
